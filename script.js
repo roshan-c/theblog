929cf46dc9cc1ca56fdf02583a08d6243bc8da5b
roshan-c <hhhhhhhhh333365@gmail.com>
2024-10-18 15:09:13 +0100

    updated how patchnotes appear

--- script.js
+++ script.js
@@ -20,3 +20,18 @@ var span = document.getElementsByClassName("close")[0];
 span.onclick = function() {
   modal.style.display = "none";
 } 
+
+document.addEventListener("DOMContentLoaded", function() {
+  var coll = document.getElementsByClassName("collapsible");
+  for (var i = 0; i < coll.length; i++) {
+      coll[i].addEventListener("click", function() {
+      this.classList.toggle("active");
+      var content = this.nextElementSibling;
+      if (content.style.display === "block") {
+          content.style.display = "none";
+      } else {
+          content.style.display = "block";
+      }
+      });
+  }
+  });
