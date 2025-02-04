d0aa0ef141c939fee4b41e7ed37bb41e0d2bc54f
roshan-c <hhhhhhhhh333365@gmail.com>
2025-02-04 23:07:49 +0000

    Add image configuration and update article to use dynamic image paths; remove unused index.js

---
+++ imageConfig.js
@@ -0,0 +1,23 @@
+const imageConfig = {
+    // Destiny related images
+    'pantheon-riven': 'images/destiny/pantheonriven.png',
+    'afk-warning': 'images/destiny/afkwarning.png',
+    'tonics-screen': 'images/destiny/tonics.avif',
+    'reddit-top': 'images/destiny/toppagereddit.png',
+    
+    // TMZ article images
+    'gart-dead': 'images/tmz/gart (720).png',
+    'overdeliver': 'images/tmz/overdeliver (720).png',
+    
+    // Tierlist images 
+    'tierlist-full': 'images/tierlist/tierlist2x.png'
+};
+
+function getImagePath(nickname) {
+    const path = imageConfig[nickname];
+    if (!path) {
+        console.error(`Image nickname "${nickname}" not found in config`);
+        return '';
+    }
+    return path;
+}
